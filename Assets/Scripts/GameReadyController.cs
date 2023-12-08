e0f5ea0bc99ebaefe11c48a1ce965e890bd1f5b5
Daniel <64601261+K33pQu13t@users.noreply.github.com>
2023-12-08 21:19:43 +0700

    Исправить вызов GameReadyAPI (#30)

---
+++ Assets/Scripts/GameReadyController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+#if UNITY_WEBGL
+using System.Runtime.InteropServices;
+#endif
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Контроллер готовности игры
+    /// </summary>
+    public class GameReadyController : MonoBehaviour
+    {
+        private GameReadyController _instance;
+
+        private void Start()
+        {
+            if (_instance == this)
+            {
+                return;
+            }
+
+            _instance = this;
+
+#if UNITY_WEBGL
+            OnGameReady();
+#endif
+        }
+
+#if UNITY_WEBGL
+        [DllImport("__Internal")]
+        private static extern void OnGameReady();
+#endif
+    }
+}
